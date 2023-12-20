445b4c7282ea0af362a84227ca51b4ff46317e36
utphuong123 <utphuong0806@gmail.com>
2023-12-20 20:55:05 +0700

    thongke

--- src/main/java/com/example/postgresdemo/controller/controller.java
+++ src/main/java/com/example/postgresdemo/controller/controller.java
@@ -95,7 +95,7 @@ public class controller {
     }
 
     @RequestMapping("/search")
-    public String searchAndPage(Model model,
+    public String search(Model model,
             @RequestParam("keywords") Optional<String> kw) {
         String kwords = kw.orElse(session.get("keywords", ""));
         session.set("keywords", kwords);
